e1a3f668902f28f7ba66bdcbb018bad561850b20
harisikhsan <harisikhsan7@gmail.com>
2020-11-16 20:17:12 +0700

    Tugas

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -5,17 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Barang extends Model
+class Transaksi extends Model
 {
     use HasFactory;
-
-    protected $table = "barang";
-
-    public function kategori(){
-        return $this->belongsTo('App\Models\Kategori');
-    }
+    protected $table='transaksi';
+    public $timestamps = false;
 
     public function transaksi_detail(){
         return $this->hasMany('App\Models\Transaksi_detail');
+        
     }
 }
---
+++ app/Models/Transaksi_detail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi_detail extends Model
+{
+    use HasFactory;
+    protected $table='transaksi_detail';
+    public $timestamps = false;
+
+    public function barangs(){
+        return $this->belongsTo('App\Models\Barang');
+    }
+
+    public function transaksi(){
+        return $this->belongsTo('App\Models\Transaksi');
+    }
+
+}
